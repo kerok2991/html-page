654bba8adfe65b437ca4e8a819b5831648efe524
KYRILLOS KHALIL <kerok2991@gmail.com>
2021-09-02 23:52:03 -0500

    javascript

---
+++ app1.js
@@ -0,0 +1,49 @@
+// console.log('Ruff Ruff')
+
+// String
+// let stringVariable = 'Dogs are a MUST'
+
+// Int / number
+// let numberVariable = 8
+
+// Boolean
+// let boolearVariable = true
+
+// stringVariable = 'Nay on the cats!'
+
+// console.log(stringVariable)
+
+function getUserName(){
+    let userName = prompt('Please Enter your Name: ');
+
+    if(userName == 'kyrillos'){
+        document.write('Ruff dog whisperer ' + userName);
+    } else {
+        document.write(userName);
+    }
+}
+
+function firsttimedogparent(){
+    let userAnswer = prompt('Are you a first time dog parent?');
+    console.log(userAnswer);
+    if(userAnswer.toLowerCase() == 'yes'){
+        let url = "https://i.pinimg.com/736x/68/e4/c1/68e4c12f5d4bdb42f584c41ce32bc5d5.jpg";
+        document.write('<img src="' + url + '">');
+    } 
+}
+
+function randomNumberGenerator(){
+    // Got the folowing code from W3 Schools
+    // https://www.w3schools.com/js/js_random.asp
+    let correctAnswer = Math.floor(Math.random() * 100) +1;
+    return correctAnswer;
+}
+
+function userGuessingGame(correctAnswer){
+    userAnswer = prompt("Guess a number between 1 and 100");
+    if(userAnswer == correctAnswer){
+        alert('You got it right');
+    } else {
+        alert('Sorry, that answer is incorrect');
+    }
+}
